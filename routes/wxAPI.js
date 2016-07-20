7beb5a778cb1727cb7eb32864bc58d15a15fe176
lotuslwb <837531387@qq.com>
2016-07-20 23:33:13 +0800

    wx js sdk

--- routes/wxAPI.js
+++ routes/wxAPI.js
@@ -1,11 +1,66 @@
 var express = require('express');
 var router = express.Router();
 
+var APPID = 'wxa60ff9366a44a254';
+var APPSECRET = '6e362e213f9cc282f5ecf913eafa18d1';
+
 //引入OAuth并实例化
 var OAuth = require('wechat-oauth');
-var client = new OAuth('wxa60ff9366a44a254', '6e362e213f9cc282f5ecf913eafa18d1');
+var client = new OAuth(APPID, APPSECRET);
+
+//微信API
+var WechatAPI = require('wechat-api');
+var api = new WechatAPI(APPID, APPSECRET);
+
+//微信sdk签名算法
+var sign = require('../module/wx/sign');
+
+
+var http = require('https');
+
+router.get('/jsSDK', function (req, res, next) {
+
+    var url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + APPID + '&secret=' + APPSECRET;
+
+    http.get(url, function (ress) {
+        ress.on('data', function (chunk) {
+            chunk = JSON.parse(chunk)
+            var TOKEN = chunk.access_token;
+            var expiresTime = chunk.expires_in;
+
+            console.log(TOKEN)
+
+            var singInUrl = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=' + TOKEN + '&type=jsapi'
+            http.get(singInUrl, function (ress) {
+                ress.on('data', function (data) {
+                    data = JSON.parse(data);
+                    var originalUrl = 'http://' + req.hostname + ':8000' + req.originalUrl;
+                    console.log(data.ticket);
+                    console.log(originalUrl);
+                    var wxConfig = sign(data.ticket, originalUrl);
+                    wxConfig.appId = APPID;
+                    res.render('wxAPI', {title: '测试微信SDK', wxConfig: wxConfig});
+                    
+                })
+            });
+        });
+    }).on('error', function (e) {
+        console.log(e);
+    }).end();
+
+
+})
+
 
 //主要是负责OAuth认证
+// router.get('/', function (req, res, next) {
+//     var hostname = req.hostname;
+//     console.log('http://' + hostname + ':8000/wx/callback')
+//     var url = client.getAuthorizeURL('http://' + hostname + ':8000/wx/callback', '', 'snsapi_userinfo');
+//
+//     res.redirect(url);
+// });
+
 router.get('/', function (req, res, next) {
     var hostname = req.hostname;
     console.log('http://' + hostname + ':8000/wx/callback')
