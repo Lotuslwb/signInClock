65cced27e44bb600e252019f1f981ac745be7e5a
lotuslwb <837531387@qq.com>
2017-01-10 11:04:45 +0800

    debug wx sdk

--- module/wx/WXConfig.js
+++ module/wx/WXConfig.js
@@ -6,8 +6,8 @@
 
 //英孚 服务号
 var WXConfig = {
-    APPID: 'wxbce8d1f919678fa0',
-    APPSECRET: 'a313091a761146d572c814e83a5c4d33'
+    APPID: 'wxa60ff9366a44a254',
+    APPSECRET: '6e362e213f9cc282f5ecf913eafa18d1'
 }
 
 
