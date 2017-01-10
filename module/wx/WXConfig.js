975da51a9325ae085af40b9fdc5a3dcf910a7ae8
lotuslwb <837531387@qq.com>
2017-01-10 12:48:32 +0800

    wx debug

--- module/wx/WXConfig.js
+++ module/wx/WXConfig.js
@@ -4,10 +4,10 @@
 //     APPSECRET: '6e362e213f9cc282f5ecf913eafa18d1'
 // }
 
-//英孚 服务号
+//英孚 订阅号
 var WXConfig = {
-    APPID: 'wxa60ff9366a44a254',
-    APPSECRET: '6e362e213f9cc282f5ecf913eafa18d1'
+    APPID: 'wxd49bea1e592f6e44',
+    APPSECRET: '75f67f0ca4ed4bcd10d2e6ce9baef3b7'
 }
 
 
