d65e28007536ea5a4b3f8b2ac7734806cdf53484
lotuslwb <837531387@qq.com>
2017-01-13 17:39:02 +0800

    debug

--- module/wx/getUserInfoByOpenid.js
+++ module/wx/getUserInfoByOpenid.js
@@ -21,14 +21,13 @@ var getUserInfoByOpenid = function (option, callback) {
     //获取userInfo的access_token;
     var openid = option.openid;
     var url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + APPID + '&secret=' + APPSECRET;
-    log(openid);
+
     load(loadWay, url, function (chunk) {
 
         var access_token = chunk.access_token;
         var expires_in = chunk.expires_in;
 
         var getInfoUrl = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token=' + access_token + '&openid=' + openid + '&lang=zh_CN';
-        log(getInfoUrl);
         load(loadWay, getInfoUrl, function (chunk) {
             callback && callback(chunk);
         });
