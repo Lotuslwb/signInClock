33966c10a56c935eb4240167fc19c374987b1ce2
lotuslwb <837531387@qq.com>
2017-01-16 12:18:00 +0800

    wx 开发

--- app.js
+++ app.js
@@ -49,7 +49,53 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 //微信服务器配置
-app.use('/wxServer', wx);
+//app.use('/wxServer', wx);
+
+var wxConfig = require('../module/wx/WXConfig');
+var wechat = require('wechat');
+var config = {
+    token: wxConfig.token,
+    appid: wxConfig.APPID,
+    encodingAESKey: wxConfig.encodingAESKey
+};
+app.use(express.query());
+app.use('/wxServer', wechat(config, function (req, res, next) {
+    // 微信输入信息都在req.weixin上
+    var message = req.weixin;
+    if (message.FromUserName === 'diaosi') {
+        // 回复屌丝(普通回复)
+        res.reply('hehe');
+    } else if (message.FromUserName === 'text') {
+        //你也可以这样回复text类型的信息
+        res.reply({
+            content: 'text object',
+            type: 'text'
+        });
+    } else if (message.FromUserName === 'hehe') {
+        // 回复一段音乐
+        res.reply({
+            type: "music",
+            content: {
+                title: "来段音乐吧",
+                description: "一无所有",
+                musicUrl: "http://mp3.com/xx.mp3",
+                hqMusicUrl: "http://mp3.com/xx.mp3",
+                thumbMediaId: "thisThumbMediaId"
+            }
+        });
+    } else {
+        // 回复高富帅(图文回复)
+        res.reply([
+            {
+                title: '你来我家接我吧',
+                description: '这是女神与高富帅之间的对话',
+                picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+                url: 'http://nodeapi.cloudfoundry.com/'
+            }
+        ]);
+    }
+}));
+
 
 app.use('/', routes);
 app.use('/users', users);
@@ -101,8 +147,6 @@ app.use(function (err, req, res, next) {
         error: {}
     });
 });
-
-
 
 
 module.exports = app;
--- routes/wxConnect.js
+++ routes/wxConnect.js
@@ -4,7 +4,6 @@ var app = express();
 
 
 var checkSignature = require('../module/wx/WXcheckSignature');
-var wxConfig = require('../module/wx/WXConfig');
 
 
 /* GET home page. */
@@ -21,51 +20,6 @@ var wxConfig = require('../module/wx/WXConfig');
 //     }
 // });
 
-var wxConfig = require('../module/wx/WXConfig');
-var wechat = require('wechat');
-var config = {
-    token: wxConfig.token,
-    appid: wxConfig.APPID,
-    encodingAESKey: wxConfig.encodingAESKey
-};
-
-app.use(express.query());
-app.use('/', wechat(config, function (req, res, next) {
-    // 微信输入信息都在req.weixin上
-    var message = req.weixin;
-    if (message.FromUserName === 'diaosi') {
-        // 回复屌丝(普通回复)
-        res.reply('hehe');
-    } else if (message.FromUserName === 'text') {
-        //你也可以这样回复text类型的信息
-        res.reply({
-            content: 'text object',
-            type: 'text'
-        });
-    } else if (message.FromUserName === 'hehe') {
-        // 回复一段音乐
-        res.reply({
-            type: "music",
-            content: {
-                title: "来段音乐吧",
-                description: "一无所有",
-                musicUrl: "http://mp3.com/xx.mp3",
-                hqMusicUrl: "http://mp3.com/xx.mp3",
-                thumbMediaId: "thisThumbMediaId"
-            }
-        });
-    } else {
-        // 回复高富帅(图文回复)
-        res.reply([
-            {
-                title: '你来我家接我吧',
-                description: '这是女神与高富帅之间的对话',
-                picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
-                url: 'http://nodeapi.cloudfoundry.com/'
-            }
-        ]);
-    }
-}));
 
 
 module.exports = router;
