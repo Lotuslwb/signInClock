0e4bd7b05807ebb274eda0d22e7a6e18b599efc3
liwenbin <837531387@qq.com>
2017-10-23 15:26:29 +0800

    debugger

--- module/wx/WXFunc.js
+++ module/wx/WXFunc.js
@@ -1,30 +1,8 @@
 var WXFunc = function (req, res, next) {
     // 微信输入信息都在req.weixin上
     var message = req.weixin;
-    console.log(message);
-    if (message.FromUserName === 'diaosi') {
-        // 回复屌丝(普通回复)
-        res.reply('hehe');
-    } else if (message.FromUserName === 'text') {
-        //你也可以这样回复text类型的信息
-        res.reply({
-            content: 'text object',
-            type: 'text'
-        });
-    } else if (message.FromUserName === 'hehe') {
-        // 回复一段音乐
-        res.reply({
-            type: "music",
-            content: {
-                title: "来段音乐吧",
-                description: "一无所有",
-                musicUrl: "http://mp3.com/xx.mp3",
-                hqMusicUrl: "http://mp3.com/xx.mp3",
-                thumbMediaId: "thisThumbMediaId"
-            }
-        });
-    } else {
-        res.reply('欢迎来到英孚教育青少儿英语服务号');
+    if (message.Event === 'subscribe') {
+        res.reply('耶，小E又交到新朋友啦！<br/>孩子学习英语遇到问题？请点击家长课堂，总有一款回答适合你。<br/>不知道如何取一个适合孩子的英文名？点击英文名神器，一键获取孩子的完美英文名！<br/>本月注册免费试听课，即获500元学习大礼包，更可免费获取胡歌电子签名版亲子英语杂志一本！');
     }
 };
 
