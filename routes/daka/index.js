09c99e17e3b4472ca7e64897117d145684bb644b
liwenbin <837531387@qq.com>
2017-10-29 14:48:31 +0800

    debugger

--- routes/daka/index.js
+++ routes/daka/index.js
@@ -1,30 +1,57 @@
 var express = require('express');
 var router = express.Router();
-
+var app = express();
 
 var WXConfig = require('../../module/wx/WXConfig');
 var APPID = WXConfig.APPID;
 
-//检查openid
-router.get('/', function (req, res, next) {
 
-  var openid = req.signedCookies['session'];
+router.get('/start', function (req, res, next) {
+    res.render('daka/start', {title: '开始打卡'});
+})
 
-  if (openid) {
-    next();
-  } else {
-    //如果cookie里面没有openid,获取之;
-    var hostname = 'ma.eldesign.cn';
-    var redirect_uri = encodeURIComponent('http://' + hostname + '/wx/callback?router=daka');
-    var url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + APPID + '&redirect_uri=' + redirect_uri + '&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect';
-    res.redirect(url);
-  }
+
+router.get('/plan', function (req, res, next) {
+    res.render('daka/plan', {title: 'index'});
+});
+
+router.get('/start_detail', function (req, res, next) {
+    res.render('daka/start_detail', {title: 'index'});
+});
+
+router.get('/personal', function (req, res, next) {
+    res.render('daka/personal', {title: 'index'});
 });
 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('daka/test', { title: 'Express' });
+router.get('/reading', function (req, res, next) {
+    res.render('daka/reading', {title: 'index'});
 });
+
+
+//*** 需要openid的页面 放在这个下面 ***//
+
+function checkOpenid(req, res, cb) {
+    var openid = req.signedCookies['session'];
+
+    if (openid) {
+        next();
+    } else {
+        //如果cookie里面没有openid,获取之;
+        var hostname = 'ma.eldesign.cn';
+        var redirect_uri = encodeURIComponent('http://' + hostname + '/wx/callback?router=daka');
+        var url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + APPID + '&redirect_uri=' + redirect_uri + '&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect';
+        res.redirect(url);
+    }
+}
+
+
+router.get('/index', function (req, res, next) {
+    checkOpenid(req, res, function () {
+        res.render('daka/index', {title: 'index'});
+    });
+
+});
+
 
 module.exports = router;
