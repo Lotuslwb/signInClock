508d10558cedc7213e3ddf12c6acf9e19b9e75bd
liwenbin <837531387@qq.com>
2017-10-31 15:53:46 +0800

    debugger

--- routes/daka/api.js
+++ routes/daka/api.js
@@ -17,6 +17,129 @@ router.get('/getUserInfo', function (req, res, next) {
     })
 });
 
+//签到
+router.get('/setSignIn', function (req, res, next) {
+    var openid = req.signedCookies['session'];
+    var recordServerId = req.query.serverId;
+    console.log(openid, 'setSignIn');
+    getUserInfoFormDB(openid, function (docs) {
+
+
+        var data = docs[0];
+        var recodeInfo = data.recodeInfo;
+        var currentRecodeCounts = recodeInfo.currentRecodeCounts * 1 || 0;
+        var currentSerialRecodeCounts = recodeInfo.currentSerialRecodeCounts * 1 || 0;
+        var lastRecodeTime = recodeInfo.lastRecodeTime;
+        var totalRecodeCounts = recodeInfo.totalRecodeCounts * 1 || 0;
+        var recodeTimeArray = readingInfo.recodeTimeArray || [];
+        var readingInfo = data.readingInfo;
+        if (lastRecodeTime.length > 0) {
+            lastRecodeTime = new Date(lastRecodeTime * 1);
+            if (isToday(lastRecodeTime)) {
+                //上次打卡时间为今天;那么就不能再打卡了
+                res.send(sendData('990', data, '你今天已经打过卡了哦'));
+                return false;
+
+            } else {
+                //打卡逻辑
+
+                //记录打卡时间
+                recodeTimeArray.push(getFormatDate());
+                readingInfo.push();
+                //记录录音信息 和 书籍信息
+                readingInfo.push({
+                    readingTimeId: getFormatDate(), //阅读日期  20170102
+                    recordServerId: recordServerId, // 录音,微信服务器ID
+                    recordLocalId: '' //录音 本地服务器ID
+                })
+
+                if (isYesterday(lastRecodeTime)) {
+                    //上次打卡时间为昨天;那么就可以统计连续打卡
+
+                    lastRecodeTime = new Date();
+                    totalRecodeCounts++;
+                    currentSerialRecodeCounts++;
+                    currentRecodeCounts++;
+                } else {
+                    lastRecodeTime = new Date();
+                    totalRecodeCounts++;
+                    currentRecodeCounts++;
+
+                    //连续打卡次数归零
+                    currentSerialRecodeCounts = 0;
+                }
+
+            }
+        } else {
+            //上次打卡时间为空 说明是第一次进来打卡
+            lastRecodeTime = new Date();
+            totalRecodeCounts++;
+            currentSerialRecodeCounts++;
+            currentRecodeCounts++;
+        }
+
+        var updateDate = {
+            recodeInfo: {
+                currentRecodeCounts: currentRecodeCounts,
+                currentSerialRecodeCounts: currentSerialRecodeCounts,
+                lastRecodeTime: lastRecodeTime.getTime(),
+                totalRecodeCounts: totalRecodeCounts,
+                recodeTimeArray: recodeTimeArray
+            },
+            readingInfo: {}
+        }
+
+        updateUserInfoToDB(data._id, updateDate, function (docs) {
+            //成功
+            res.send(sendData('200', updateDate, ''));
+        }, function (docs) {
+            //失败
+            res.send(sendData('999', docs, '数据库更新失败'));
+        })
+
+
+    }, function (docs) {
+        res.cookie('session', '');
+        res.send(sendData('990', docs, '暂无此用户的信息,请刷新重试'));
+    })
+
+});
+
+
+function getUserInfoFormDB(openid, callback_s, callback_f) {
+
+    if (!openid) {
+        callback_f && callback_f('openid 不能为空');
+    }
+    var UserDB = require('../module/DB/UserDB');
+    var findJSON = {
+        openid: openid.split('"')[1]
+    };
+
+
+    UserDB.find(findJSON).then(function (docs) {
+        if (docs.length > 0) {
+            log('---数据库里面已经有此用户---');
+            callback_s && callback_s(docs);
+        } else {
+            log('---数据库里面暂无此用户---');
+            callback_f && callback_f(docs);
+        }
+    });
+}
+
+function updateUserInfoToDB(_id, data, callback_s, callback_f) {
+    var UserDB = require('../module/DB/UserDB');
+
+    UserDB.update(_id, data, function (err, docs) {
+        if (err) {
+            callback_f && callback_f(docs);
+        } else {
+            callback_s && callback_s(docs);
+        }
+    });
+}
+
 function getUserInfoByOpenid(openid, cb_s, cb_f) {
     if (!openid) {
         cb_f && cb_f('openid 不能为空');
