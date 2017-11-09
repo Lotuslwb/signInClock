9d4d15480c83d2abd8f988913c8ca01431f22962
liwenbin <837531387@qq.com>
2017-11-09 10:50:35 +0800

    debugger

---
+++ bin/sendMessage/double11.js
@@ -0,0 +1,36 @@
+//设置批量发送消息
+var sendMessageBath = require('../sendMessageBath');
+var openIdList = ["oKdUIuK-J2-m8ftz_adGLyTmZ2aY", 'oKdUIuDXWO5Ek3IswpcRvESoOUVI', "oKdUIuHCbs97GlnTte7V6Yj_IG34", "oKdUIuGgokkiL4du7fC9rfdRQGrg"];
+var data = {
+    "touser": '',  //接收者openid
+    "template_id": "hEUpEGx8MV2mC5gs7lUJ5954esygcPCAYPWq90YshQ8", //模板ID
+    "url": "http://www.ef.com.cn/englishfirst/landing/mobilelifeclub?etag=EFCN_Win18_KidsOwn-SNS-WechatSA-message-double11",
+    "data": {
+        "first": {
+            "value": "英孚青少儿英语2017秋季课程,11.11超值优惠！！",
+            "color": "#173177"
+        },
+        "keyword1": {
+            "value": "年度巨献 - 新生奖学金",
+            "color": "#173177"
+        },
+        "keyword2": {
+            "value": "梁芳莹Fandy",
+            "color": "#173177"
+        },
+        "remark": {
+            "value": "立即领取",
+            "color": "#173177"
+        }
+    }
+};
+
+sendMessageBath(openIdList, data);
+
+
+
+
+
+
+
+
