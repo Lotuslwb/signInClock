c2db353cd1e3efc79a58665510ce13ddda6a3b81
liwenbin <837531387@qq.com>
2017-11-09 16:28:47 +0800

    debugger

--- bin/daka/sendMessage.js
+++ bin/daka/sendMessage.js
@@ -56,15 +56,13 @@ function getOpenIdList(cb) {
         console.log(docs, 'docs');
         var now = new Date();
         var now_hours = now.getHours();
-        var now_minutes = now.getHours();
+        var now_minutes = now.getMinutes();
 
         for (var i = 0; i < docs.length; i++) {
             var doc = docs[i];
             var doc_time = doc['clockInfo']['clockTime'];
             var doc_hours = doc_time.split(':')[0];
             var doc_minutes = doc_time.split(':')[1];
-            console.log(doc_hours,now_hours);
-            console.log(doc_minutes,now_minutes);
             if (now_hours == doc_hours && Math.abs(now_minutes - doc_minutes) <= 5) {
                 var openid = doc['openid'];
                 openIdList.push(openid);
