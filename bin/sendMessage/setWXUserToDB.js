08317f31af0603b8e5ddc6aeea46c30acd74bf43
liwenbin <837531387@qq.com>
2017-11-10 15:50:55 +0800

    debugger

--- bin/sendMessage/setWXUserToDB.js
+++ bin/sendMessage/setWXUserToDB.js
@@ -113,7 +113,8 @@ function getOpenIdList(callback) {
 //同步微信和本地数据库数据
 function synDataInWxAndDB() {
     getOpenIdList(function (openIdList, access_token) {
-        WXUserDB.find({}).then(function (docs) {
+        WXUserDB.find({}, function (docs) {
+            console.log(docs.length);
             var openIdListInDB = docs.map(function (item) {
                 return item.openid;
             });
