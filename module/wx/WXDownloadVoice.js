d833bf7ec84664c1eb4acf8e8c0c216f31829fa3
liwenbin <837531387@qq.com>
2017-11-14 20:33:09 +0800

    debugger

---
+++ module/wx/WXDownloadVoice.js
@@ -0,0 +1,21 @@
+var getWebContent = require('../../module/tools/getWebContent');
+var getSDKSign = require('../../module/wx/getSDKSign');
+
+var originalUrl = '';
+
+getSDKSign(originalUrl, function (wxConfig) {
+    var access_token = wxConfig['TOKEN'];
+    var url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=' + access_token;
+    getWebContent(url, 'POST', data, function (response) {
+        cb && cb(response);
+        //console.log(response.body);
+        if (response.body.errcode == '0') {
+            // console.log('---消息推送 成功--');
+        }
+    });
+});
+
+
+module.exports = function (data, cb) {
+    
+}
