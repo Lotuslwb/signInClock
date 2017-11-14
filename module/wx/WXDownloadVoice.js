da12ad9d0e9bedb9ecf72eedb53897258991d754
liwenbin <837531387@qq.com>
2017-11-14 20:43:10 +0800

    debugger

--- module/wx/WXDownloadVoice.js
+++ module/wx/WXDownloadVoice.js
@@ -2,16 +2,15 @@ var getWebContent = require('../../module/tools/getWebContent');
 var getSDKSign = require('../../module/wx/getSDKSign');
 
 var originalUrl = '';
-var medieId = 'wxLocalResource://voiceLocalId1234567890123';
+var mediaId = '1237378768e7q8e7r8qwesafdasdfasdfaxss111';
 getSDKSign(originalUrl, function (wxConfig) {
     var access_token = wxConfig['TOKEN'];
-    var url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=' + access_token;
-    getWebContent(url, 'POST', data, function (response) {
-        cb && cb(response);
-        //console.log(response.body);
-        if (response.body.errcode == '0') {
-            // console.log('---消息推送 成功--');
-        }
+    var url = 'https://api.weixin.qq.com/cgi-bin/media/get?access_token=' + access_token + '&media_id=' + mediaId;
+    getWebContent(url, 'GET', '', function (response) {
+        console.log(response.body);
+        // if (response.body.errcode == '0') {
+        //     // console.log('---消息推送 成功--');
+        // }
     });
 });
 
