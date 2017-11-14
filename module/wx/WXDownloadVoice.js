32995f9af556d752445e0d02f2e9ebc5da426237
liwenbin <837531387@qq.com>
2017-11-14 20:54:03 +0800

    debugger

--- module/wx/WXDownloadVoice.js
+++ module/wx/WXDownloadVoice.js
@@ -2,7 +2,7 @@ var getWebContent = require('../../module/tools/getWebContent');
 var getSDKSign = require('../../module/wx/getSDKSign');
 
 var originalUrl = '';
-var mediaId = '1237378768e7q8e7r8qwesafdasdfasdfaxss111';
+var mediaId = 'wPaWv7itm1ZPNknhTU9gwlXDfDl7WhCQsG7mRR2b8k2Q3Fl-wt5uEqBLK4Jr57uM';
 getSDKSign(originalUrl, function (wxConfig) {
     var access_token = wxConfig['TOKEN'];
     var url = 'https://api.weixin.qq.com/cgi-bin/media/get?access_token=' + access_token + '&media_id=' + mediaId;
