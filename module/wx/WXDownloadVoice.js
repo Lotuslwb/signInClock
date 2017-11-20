4923536c47086e38f8cf69f975c0bae615f38e6b
liwenbin <837531387@qq.com>
2017-11-20 09:51:48 +0800

    debugger

--- module/wx/WXDownloadVoice.js
+++ module/wx/WXDownloadVoice.js
@@ -1,6 +1,5 @@
 var getWebContent = require('../../module/tools/getWebContent');
 var getSDKSign = require('../../module/wx/getSDKSign');
-var fs = require('fs');
 
 var originalUrl = '';
 var mediaId = '1Mzulsh1vcveqylz0Ir6REd9OH2RZDIU72Icl2cv1tVm57qXb4dy5Tm-n-NFgt0B';
@@ -8,12 +7,27 @@ getSDKSign(originalUrl, function (wxConfig) {
     var access_token = wxConfig['TOKEN'];
     var url = 'https://api.weixin.qq.com/cgi-bin/media/get?access_token=' + access_token + '&media_id=' + mediaId;
     console.log(url);
-    getWebContent(url, 'GET', '', function (response) {
-        response.body.pipe(fs.createWriteStream(mediaId));
-        // if (response.body.errcode == '0') {
-        //     // console.log('---消息推送 成功--');
-        // }
+
+
+    var fs = require('fs');
+    var request = require('request');
+//采用request模块，向服务器发起一次请求，获取图片资源
+    request.head(url, function (err, res, body) {
+        if (err) {
+            console.log(err);
+        }
     });
+
+    var img_filename = 'mu.jpg';
+    request(url).pipe(fs.createWriteStream('./' + img_filename));     //通过流的方式，把图片写到本地目录下
+
+
+    // getWebContent(url, 'GET', '', function (response) {
+    //     response.body.pipe(fs.createWriteStream(mediaId));
+    //     // if (response.body.errcode == '0') {
+    //     //     // console.log('---消息推送 成功--');
+    //     // }
+    // });
 });
 
 
