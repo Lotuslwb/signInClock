8a80d6505661f31f305995eb42245824170c1555
liwenbin <837531387@qq.com>
2017-11-20 11:08:04 +0800

    debugger

--- module/wx/WXDownloadVoice.js
+++ module/wx/WXDownloadVoice.js
@@ -2,9 +2,8 @@ var getWebContent = require('../../module/tools/getWebContent');
 var getSDKSign = require('../../module/wx/getSDKSign');
 
 var DOWNLOAD_DIR = '/';
-var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 var fs = require('fs');
-var url = require('url');
 
 
 var originalUrl = '';
@@ -13,42 +12,21 @@ getSDKSign(originalUrl, function (wxConfig) {
     var access_token = wxConfig['TOKEN'];
     var url = 'https://api.weixin.qq.com/cgi-bin/media/get?access_token=' + access_token + '&media_id=' + mediaId;
     console.log(url);
-
-
-    download_file_curl(url)
-
-    // getWebContent(url, 'GET', '', function (response) {
-    //     response.body.pipe(fs.createWriteStream(mediaId));
-    //     // if (response.body.errcode == '0') {
-    //     //     // console.log('---消息推送 成功--');
-    //     // }
-    // });
+    download_file_curl(url, mediaId);
 });
 
 
 // Function to download file using curl
-var download_file_curl = function (file_url) {
-
-    // extract the file name
-    var file_name = url.parse(file_url).pathname.split('/').pop();
-    // create an instance of writable stream
-    var file = fs.createWriteStream(DOWNLOAD_DIR + file_name);
-    // execute curl using child_process' spawn function
-    var curl = spawn('curl', [file_url]);
-    // add a 'data' event listener for the spawn instance
-    curl.stdout.on('data', function (data) {
-        file.write(data);
-    });
-    // add an 'end' event listener to close the writeable stream
-    curl.stdout.on('end', function (data) {
-        file.end();
-        console.log(file_name + ' downloaded to ' + DOWNLOAD_DIR);
-    });
-    // when the spawn child process exits, check if there were any errors and close the writeable stream
-    curl.on('exit', function (code) {
-        if (code != 0) {
-            console.log('Failed: ' + code);
+var download_file_curl = function (file_url, mediaId) {
+    exec('curl -o ' + mediaId + '.amr "' + file_url + '"', function (err, stdout, stderr) {
+        if (err) {
+            console.log(error.stack);
+            console.log('Error code: ' + error.code);
+            console.log('Signal received: ' + error.signal);
         }
+        console.log('data : ' + stdout);
+    }).on('exit', function (code) {
+        console.log('子进程已退出, 退出码 ' + code);
     });
 };
 
