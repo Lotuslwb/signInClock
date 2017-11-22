a8f87fce6314b13f19f32cefd15348b63c73065b
liwenbin <837531387@qq.com>
2017-11-22 20:53:02 +0800

    debugger

--- bin/daka/downloadVoice.js
+++ bin/daka/downloadVoice.js
@@ -1,23 +1,41 @@
 //设置定时任务 变量
 var schedule = require("node-schedule");
 var rule2 = new schedule.RecurrenceRule();
-var times2 = [0, 30];
-rule2.minute = times2;
+var times2 = [1]; // 以小时为单位,每晚1点运行
+rule2.hour = times2;
 
 // 查询数据库
 var fs = require('fs');
 var UserDB = require('../../module/DB/UserDB');
 var wxDownloadVoicePromise = require('../../module/wx/WXDownloadVoice');
+var DOWNLOAD_DIR = '/root/signInClock/public/files/media/';
 
 schedule.scheduleJob(rule2, function () {
+    getMediaIdObjList(function (MediaIdObjList) {
+        fs.unlinkSync('access_token.txt');
+        var MediaIdObjPromiseList = MediaIdObjList.map(function (item) {
+            var downloadPromiseArray = item.mediaIdList.map(function (mediaId) {
+                return wxDownloadVoicePromise({
+                    DOWNLOAD_DIR: DOWNLOAD_DIR,
+                    mediaId: mediaId
+                });
+            });
+            return Promise.all(downloadPromiseArray).then(function (data) {
+                for (var i = 0; i < data.length; i++) {
+                    item['readingInfo'][i]['recordLocalId'] = data[i];
+                }
 
+                return UserDB.User.update({'openid': item.openid}, {'readingInfo': item.readingInfo});
+            })
+        });
+        Promise.all(MediaIdObjPromiseList).then(function (allData) {
+            console.log(allData, 'allData');
+        });
+    });
 });
-
-var DOWNLOAD_DIR = '/root/signInClock/public/files/media/';
 
 getMediaIdObjList(function (MediaIdObjList) {
     fs.unlinkSync('access_token.txt');
-    var MediaIdObjOutputList;
     var MediaIdObjPromiseList = MediaIdObjList.map(function (item) {
         var downloadPromiseArray = item.mediaIdList.map(function (mediaId) {
             return wxDownloadVoicePromise({
