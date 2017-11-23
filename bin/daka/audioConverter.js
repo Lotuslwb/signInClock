75f6ffeaf8a1685b28ddbe0b00ae9a13a836c8d5
liwenbin <837531387@qq.com>
2017-11-23 14:43:23 +0800

    debugger

--- bin/daka/audioConverter.js
+++ bin/daka/audioConverter.js
@@ -1,6 +1,6 @@
 var path = '/root/signInClock/public/files/media';
 var ffmpeg = require('fluent-ffmpeg');
-var command = ffmpeg(path + '/1Mzulsh1vcveqylz0Ir6REd9OH2RZDIU72Icl2cv1tVm57qXb4dy5Tm-n-NFgt0B.amr')
+var command = ffmpeg(path + '/0fY6gM-wmrptU7tgGhlCU6QPHXIuna2cwkJbwWa_m7Xvxak8lz8nFIHll3r6R3OB.amr')
     .on('end', function () {
         console.log('file has been converted succesfully');
     })
