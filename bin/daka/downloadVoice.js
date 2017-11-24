1291afd640316cf03053312d6363c0e0fe76bc25
liwenbin <837531387@qq.com>
2017-11-24 15:51:42 +0800

    debugger

--- bin/daka/downloadVoice.js
+++ bin/daka/downloadVoice.js
@@ -34,14 +34,17 @@ function downloadVoice() {
             });
             return Promise.all(downloadPromiseArray).then(function (data) {
                 // 获得下载到本地音频的path list  就是data
+
                 for (var i = 0; i < data.length; i++) {
                     var recordServerId = item['readingInfo'][i]['recordServerId'];
                     if (data[i].indexOf(recordServerId) > -1) {
                         item['readingInfo'][i]['recordLocalId'] = data[i];
                     }
                 }
-                if(item.openid=='oKdUIuDXWO5Ek3IswpcRvESoOUVI'){
-                    console.log({'readingInfo': item.readingInfo},'oKdUIuDXWO5Ek3IswpcRvESoOUVI')
+                if (item.openid == 'oKdUIuDXWO5Ek3IswpcRvESoOUVI') {
+                    console.log(data, 'path list');
+
+                    console.log({'readingInfo': item.readingInfo}, 'oKdUIuDXWO5Ek3IswpcRvESoOUVI')
                 }
                 return UserDB.User.update({'openid': item.openid}, {'readingInfo': item.readingInfo});
             })
