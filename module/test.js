193dfc452e8be08fc25a7a7083aff91cfb79568d
liwenbin <837531387@qq.com>
2017-11-29 22:10:35 +0800

    merge

--- module/test.js
+++ module/test.js
@@ -1,1 +1,9 @@
+var fs = require("fs");
+var fsPath = '/root/signInClock';
+var bookId = fs.readFileSync(fsPath + '/bookId.txt');
+var ArticleDB = require('../module/DB/ArticleDB');
 
+
+ArticleDB.User.find({}, {'createTime': 1, 'articleTitle': 1}).sort({"createTime": 1}).then(function (docs) {
+    console.log(docs);
+});
