80bc7ef761443dc3740ac8be8cd33727e37a841f
liwenbin <837531387@qq.com>
2017-11-29 22:42:19 +0800

    merge

--- module/test.js
+++ module/test.js
@@ -6,7 +6,7 @@ var ArticleDB = require('../module/DB/ArticleDB');
 
 ArticleDB.User.find({}, {'createTime': 1, 'articleTitle': 1}).sort({"createTime": 1}).then(function (docs) {
     var idList = docs.map(function (doc) {
-        return doc._id;
+        return doc._id.toString();
     });
     var index = idList.indexOf(bookId);
     console.log(index, 'index');
