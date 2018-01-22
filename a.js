5dae0682cfd9681447255cf7c2272d8884904436
liwenbin <837531387@qq.com>
2018-01-22 17:21:45 +0800

    debug

---
+++ a.js
@@ -0,0 +1,16 @@
+db.users.find({}).forEach(function (x) {
+    x.readingInfo = [];
+    x.clockInfo = {
+        clockTime: '18:00',
+        clockSwitch: 'on'
+    };
+    x.recodeInfo = {
+        lastRecodeTime: '0',
+        totalRecodeCounts: '0',
+        currentRecodeCounts: '0',
+        currentSerialRecodeCounts: '0',
+        recodeTimeArray: [],
+        totalWordLength: '0'
+    };
+    db.getCollection('users').save(x);
+})
