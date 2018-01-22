3b31f7241d256e0cbcb775890fccf18da8e70900
liwenbin <837531387@qq.com>
2018-01-23 00:44:56 +0800

    debug

--- a.js
+++ a.js
@@ -1,4 +1,4 @@
-db.users.find({'personInfo.nickname':'毛伟'}).forEach(function (x) {
+db.users.find({'personInfo.nickname': '李文字彬'}).forEach(function (x) {
     x.readingInfo = [];
     x.clockInfo = {
         clockTime: '18:00',
@@ -11,6 +11,15 @@ db.users.find({'personInfo.nickname':'毛伟'}).forEach(function (x) {
         currentSerialRecodeCounts: '0',
         recodeTimeArray: [],
         totalWordLength: '0'
+    };
+    db.getCollection('users').save(x);
+})
+
+
+db.users.find().limit(40000).skip(35000).forEach(function (x) {
+    x.clockInfo = {
+        clockTime: '20:30',
+        clockSwitch: 'on'
     };
     db.getCollection('users').save(x);
 })
