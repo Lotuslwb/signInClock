66994dfa0e0bb1f38e5437cdaa155bdb5a3f6afc
liwenbin <837531387@qq.com>
2018-01-22 11:04:16 +0800

    debug

---
+++ a.js
@@ -0,0 +1,210 @@
+{
+    "__v"
+:
+    0, "_id"
+:
+    ObjectId("5a56dfdc1f563f3cdef4d661"), "clockInfo"
+:
+    {
+        "clockSwitch"
+    :
+        "on", "clockTime"
+    :
+        "18:00"
+    }
+,
+    "couponList"
+:
+    [], "level"
+:
+    "1", "openid"
+:
+    "oKdUIuDXWO5Ek3IswpcRvESoOUVI", "personInfo"
+:
+    {
+        "nickname"
+    :
+        "李文字彬", "sex"
+    :
+        "1", "city"
+    :
+        "", "headimgurl"
+    :
+        "http://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLyV1Ir9mstNAOmvyNzpTFcibv3xWVSCp135dehaicLbhWY6hRDnadIibZq3V1V6PNETobaDXXLIBibMw/132", "startTime"
+    :
+        1515642844821
+    }
+,
+    "readingInfo"
+:
+    [{
+        "readingList": {
+            "bookId": "5a575a269ad8685dc845d64d",
+            "bookName": "Don&#39;t Be Late",
+            "bookCover": "/files/daka/b5bEeWJWt3ZwVmOTek_cKvWi.png",
+            "bookDes": "Ring! Ring! It&#39;s 9:30 a.m. – Elise is calling Tom. "
+        },
+        "readingTimeId": "20180112",
+        "recordServerId": "-9vmA4nFrEmzq8OCn0IkDDHJTb-I-FEHpLy2tc8QVsaE8P-iuhXlvwvCV04X15QO",
+        "recordLocalId": "/root/signInClock/public/files/media/-9vmA4nFrEmzq8OCn0IkDDHJTb-I-FEHpLy2tc8QVsaE8P-iuhXlvwvCV04X15QO.mp3",
+        "_id": ObjectId("5a57ffc9dbf8cb0a721d145a")
+    }, {
+        "readingList": {
+            "bookId": "5a575ce69ad8685dc845d651",
+            "bookName": "Lazy Saturdays",
+            "bookCover": "/files/daka/uKQBifcevARnkn_hN3ibYN6p.png",
+            "bookDes": "Today is Saturday.   Fleet and Tom need to finish their science project before Monday. "
+        },
+        "readingTimeId": "20180113",
+        "recordServerId": "00fnqycUIEyfrQTd3ESuxy46-Ny7EndrCc2_FqbgAv-BR1_oxX2XPNKMHZ6B840b",
+        "recordLocalId": "/root/signInClock/public/files/media/00fnqycUIEyfrQTd3ESuxy46-Ny7EndrCc2_FqbgAv-BR1_oxX2XPNKMHZ6B840b.mp3",
+        "_id": ObjectId("5a5952b2349b1a684885f76e")
+    }, {
+        "readingList": {
+            "bookId": "5a5763df9ad8685dc845d655",
+            "bookName": "The Sun and The Wind",
+            "bookCover": "/files/daka/Muba4ow8cEE-dOU0WVli4w7M.png",
+            "bookDes": "The sun and the wind are always fghting.&#34;I&#39;m stronger than you!&#34; says the wind to the sun. "
+        },
+        "readingTimeId": "20180114",
+        "recordServerId": "NbqJHJcM81eeWF3VmM980kMQoVgeGZbHbVpMTFO4lPKXwRKRY7pLVukM2z_4wgcO",
+        "recordLocalId": "/root/signInClock/public/files/media/NbqJHJcM81eeWF3VmM980kMQoVgeGZbHbVpMTFO4lPKXwRKRY7pLVukM2z_4wgcO.mp3",
+        "_id": ObjectId("5a5b2ad2132b7e71dd63e057")
+    }, {
+        "readingList": {
+            "bookId": "5a5769e99ad8685dc845d66a",
+            "bookName": "Today&#39;s Special",
+            "bookCover": "/files/daka/ypvVxVDMDLPLSiYuMv0098EC.png",
+            "bookDes": "Mo&#39;s still on vacation. ",
+            "level": "1"
+        },
+        "readingTimeId": "20180117",
+        "recordServerId": "xpR4G7eu43VLeYPKOLj76A53mn-EUIlVxF37g_gId1umwRVbihzZjboXAQ86j24g",
+        "recordLocalId": "/root/signInClock/public/files/media/xpR4G7eu43VLeYPKOLj76A53mn-EUIlVxF37g_gId1umwRVbihzZjboXAQ86j24g.mp3",
+        "_id": ObjectId("5a5ea4c68aadef789ff8af5f")
+    }, {
+        "readingList": {
+            "bookId": "5a576c819ad8685dc845d66d",
+            "bookName": "CatchTheBall!",
+            "bookCover": "/files/daka/4jYPG3T5-y5WqinmzBMOKHqH.png",
+            "bookDes": "Vic: What is he doing? ",
+            "level": "0"
+        },
+        "readingTimeId": "20180118",
+        "recordServerId": "YFTiIBOLMDEIq5OgZZPkO0dUv5_pwUynx4WR68IE5798uDv__yxHw3516z7rILs_",
+        "recordLocalId": "/root/signInClock/public/files/media/YFTiIBOLMDEIq5OgZZPkO0dUv5_pwUynx4WR68IE5798uDv__yxHw3516z7rILs_.mp3",
+        "_id": ObjectId("5a5fe9a102cf4960eddce911")
+    }, {
+        "readingList": {
+            "bookId": "5a5767eb9ad8685dc845d65b",
+            "bookName": "The Pizza Party",
+            "bookCover": "/files/daka/KOo-DFBQQqOIjEPM0zDvlxJX.png",
+            "bookDes": "Hmm.. I like cake. I like bread. I like pizza. I want pizza!",
+            "level": "0"
+        },
+        "readingTimeId": "20180116",
+        "recordServerId": "1237378768e7q8e7r8qwesafdasdfasdfaxss111",
+        "recordLocalId": "",
+        "_id": ObjectId("5a60146c45931673bde2ed77")
+    }, {
+        "readingList": {
+            "bookId": "5a5765d59ad8685dc845d658",
+            "bookName": "The Color Song",
+            "bookCover": "/files/daka/Z2vR-xdAkzZK_pnHT5SB5eiM.png",
+            "bookDes": "Colors, colors, colors. ",
+            "level": "0"
+        },
+        "readingTimeId": "20180115",
+        "recordServerId": "AoUAi8hMGpte2zbE_xgm7mqvYeZjTLzxx5tx1WLhNoA-KUHnVjbNP4sjCwcDF0DM",
+        "recordLocalId": "/root/signInClock/public/files/media/AoUAi8hMGpte2zbE_xgm7mqvYeZjTLzxx5tx1WLhNoA-KUHnVjbNP4sjCwcDF0DM.mp3",
+        "_id": ObjectId("5a60150045931673bde2ed78")
+    }, {
+        "readingList": {
+            "bookId": "5a5f6d6678b92c36ec778f41",
+            "bookName": "Diddle Daddle",
+            "bookCover": "/files/daka/ljEBAH5m7pohDR8hG5WZHbMm.png",
+            "bookDes": "Diddle daddle, diddle daddle ",
+            "level": "0"
+        },
+        "readingTimeId": "20180119",
+        "recordServerId": "qUhNMNo1X_GPiiwPFL37wRRa3bGHZeOSY362ONYITjxYpCVMOJLVXiiY_O77pClX",
+        "recordLocalId": "/root/signInClock/public/files/media/qUhNMNo1X_GPiiwPFL37wRRa3bGHZeOSY362ONYITjxYpCVMOJLVXiiY_O77pClX.mp3",
+        "_id": ObjectId("5a62f8a203d4fd56b6516d66")
+    }, {
+        "readingList": {
+            "bookId": "5a5f6ebf78b92c36ec778f44",
+            "bookName": "The Young King Rhyme",
+            "bookCover": "/files/daka/_L7ACxm9Ph_Pl9RVnRCj6j3-.png",
+            "bookDes": "The young king had many things ",
+            "level": "0"
+        },
+        "readingTimeId": "20180120",
+        "recordServerId": "-PnSR8QrxKEI6MgtwD111YObDEZPCx6_NwGPv_EK6NYSod-VOTEOELHRMOlEQS8V",
+        "recordLocalId": "/root/signInClock/public/files/media/-PnSR8QrxKEI6MgtwD111YObDEZPCx6_NwGPv_EK6NYSod-VOTEOELHRMOlEQS8V.mp3",
+        "_id": ObjectId("5a62f8ff03d4fd56b6516d67")
+    }, {
+        "readingList": {
+            "bookId": "5a576c819ad8685dc845d66d",
+            "bookName": "Catch The Ball!",
+            "bookCover": "/files/daka/4jYPG3T5-y5WqinmzBMOKHqH.png",
+            "bookDes": "Vic: What is he doing? Roddy: Hmm…I don&#39;t know. Is he kicking the ball? ",
+            "level": "0"
+        },
+        "readingTimeId": "20180129",
+        "recordServerId": "S-dqIXuJ4YYJ5AIdRJ5KDGIAn96mzZtbvBSZxYDiSMpMWwgfWkAlS5HoyT7FbnDU",
+        "recordLocalId": "/root/signInClock/public/files/media/S-dqIXuJ4YYJ5AIdRJ5KDGIAn96mzZtbvBSZxYDiSMpMWwgfWkAlS5HoyT7FbnDU.mp3",
+        "_id": ObjectId("5a631b3ef64833620472d9c5")
+    }, {
+        "readingList": {
+            "bookId": "5a5763df9ad8685dc845d655",
+            "bookName": "Roddy&#39;sFamily",
+            "bookCover": "/files/daka/7iv2dDmN3mt5pjCVKdQf62YO.png",
+            "bookDes": "I like your photo album. Look at my photo album! ",
+            "level": "0"
+        },
+        "readingTimeId": "20180125",
+        "recordServerId": "XfW0Pt486wrBOxpjXEfQOSmXTKfl6Mqld1tTZ5AkN5fL8EKUdM1weOstOIHHsQUu",
+        "recordLocalId": "/root/signInClock/public/files/media/XfW0Pt486wrBOxpjXEfQOSmXTKfl6Mqld1tTZ5AkN5fL8EKUdM1weOstOIHHsQUu.mp3",
+        "_id": ObjectId("5a631b4af64833620472d9c6")
+    }, {
+        "readingList": {
+            "bookId": "5a56f09e79acd04172c6c431",
+            "bookName": "Hello Song",
+            "bookCover": "/files/daka/aO6M3lGfmjWSNzgGm4uBQz9J.png",
+            "bookDes": "EF Small Star kids! Hello everyone! We are here to learn and play!",
+            "level": "0"
+        },
+        "readingTimeId": "20180122",
+        "recordServerId": "xcep6BAnnYFo1ugftNsy_dZdvkyojWW3lugFXbPSmLRaO8rr5q72q7NmmaA-NprI",
+        "recordLocalId": "",
+        "_id": ObjectId("5a652d5100424515abd8f4f8")
+    }, {
+        "readingList": {
+            "bookId": "5a5f6fa578b92c36ec778f47",
+            "bookName": "Amazing Rides",
+            "bookCover": "/files/daka/QSgRZ3nRIyHY_V2YLqauYjj7.png",
+            "bookDes": "When the roller coaster &#39;Cyclone&#39; opened in 1925, people were surprised. ",
+            "level": "1"
+        },
+        "readingTimeId": "20180121",
+        "recordServerId": "0wWxIRUXFUCfxiNTLwk72ibmrMVuUpmO00w46-DeTBqfzNK8BfJ74I_RYt37Rd-p",
+        "recordLocalId": "/root/signInClock/public/files/media/0wWxIRUXFUCfxiNTLwk72ibmrMVuUpmO00w46-DeTBqfzNK8BfJ74I_RYt37Rd-p.mp3",
+        "_id": ObjectId("5a653781a0131819fedbde22")
+    }], "recodeInfo"
+:
+    {
+        "currentRecodeCounts"
+    :
+        "8", "currentSerialRecodeCounts"
+    :
+        "0", "lastRecodeTime"
+    :
+        "1516580177816", "totalRecodeCounts"
+    :
+        "8", "recodeTimeArray"
+    :
+        ["20180112", "20180113", "20180114", "20180117", "20180118", "20180120", "20180122"], "totalWordLength"
+    :
+        "655"
+    }
+}
