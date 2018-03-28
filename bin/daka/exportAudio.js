cd26f6fea374885e2711c02fd0993a0b05a75f2a
liwenbin <837531387@qq.com>
2018-03-28 09:19:17 +0800

    debug

--- bin/daka/exportAudio.js
+++ bin/daka/exportAudio.js
@@ -1,6 +1,6 @@
 // 查询数据库
 var UserDB = require('../../module/DB/UserDB');
-var UserNameList = ['Wmm', 'Cherry'];
+var UserNameList = ['Wmm', 'Cherry', '「」jiang', '株株', 'AJack·GB', '吴晓静', 'Catherine', '冰清', 'izumi', 'Alicia', 'nicole', '咕哩咕哩', '懿', '疏影横斜', 'Luluzhuo', '红英落秋', '王卉', 'lika', 'penny', 'Emma', 'tony'];
 
 UserNameList.map(function (nickname) {
     getPersonInfo(nickname).then(function (data) {
@@ -12,22 +12,23 @@ UserNameList.map(function (nickname) {
 
 
 function getPersonInfo(nickname) {
-    return UserDB.User.find({'personInfo.nickname': eval("^/" + nickname + "/i")}, {
+    return UserDB.User.find({'personInfo.nickname': eval("/" + nickname + "/")}, {
         'openid': 1,
         'personInfo': 1,
         'readingInfo': 1,
     }).then(function (docs) {
-        console.log(docs)
-        var personInfo = docs.personInfo;
-        var recordIdList = docs.readingInfo.map(function (item) {
-            return item.recordLocalId;
-        });
-        var data = {
-            nickname: personInfo.nickname,
-            headimgurl: personInfo.headimgurl,
-            openid: openid,
-            recordIdList: recordIdList
-        };
+        var data = docs.map(function (doc) {
+            var personInfo = doc.personInfo;
+            var recordIdList = doc.readingInfo.map(function (item) {
+                return item.recordLocalId;
+            });
+            return {
+                nickname: personInfo.nickname,
+                headimgurl: personInfo.headimgurl,
+                openid: openid,
+                recordIdList: recordIdList
+            };
+        })
         return data
     });
 }
