56c73f8275f21128ab42c151d8d2e1ef350cd4ab
liwenbin <837531387@qq.com>
2018-03-29 15:57:11 +0800

    debug

--- bin/daka/test.js
+++ bin/daka/test.js
@@ -2,7 +2,7 @@ var wxDownloadVoicePromise = require('../../module/wx/WXDownloadVoice');
 
 wxDownloadVoicePromise({
     DOWNLOAD_DIR: '/root/signInClock/public/files/media/',
-    mediaId: 'Vh7BhZI25vbNVelTVawBFYYEqGQ7Wr3FUp6JQZ5G2ezkSHjXSg-QLH-28J5n_3B3'
+    mediaId: 'f_zAQiKrC5b78H7e8vAjaTGnfp_H2gMO8ohLjeX7XPELIchnm065QcX4u7UXFIlb'
 }).catch(function (e) {
     console.log(e);
 });
