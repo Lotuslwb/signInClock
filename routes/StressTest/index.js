2117333ff0a9040d0cd8c72421e5ac4c402c658d
liwenbin <837531387@qq.com>
2018-07-04 15:47:46 +0800

    压力测试 微信分享

--- routes/StressTest/index.js
+++ routes/StressTest/index.js
@@ -6,11 +6,12 @@ var StateRegion = require('./StateRegion');
 var resultList = 'female_1_1.png,female_2_3.png,female_3_4.png,male_1_1.png,male_4_1.png,female_1_2.png,female_3_1.png,female_4_1.png,male_2_1.png,female_2_1.png,female_3_2.png,female_4_2.png,male_3_1.png,female_2_2.png,female_3_3.png,female_4_3.png,male_3_2.png';
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    console.log(req.host);
+    var channel = req.query.channel;
     res.render('StressTest/index', {
         data,
         resultList,
-        StateRegion
+        StateRegion,
+        channel
     });
 });
 
