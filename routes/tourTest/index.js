90b9e2861d7dfd1a9394ced33ebc2b79d7b51a1a
liwenbin <837531387@qq.com>
2018-09-20 10:20:07 +0800

    旅游测试

--- routes/tourTest/index.js
+++ routes/tourTest/index.js
@@ -35,8 +35,11 @@ router.get('/v2', function (req, res, next) {
 });
 
 router.post('/form', function (req, res, next) {
-    // var host = 'https://stg-efcom-lb.eflangtech.com/';  
-    var host = 'https://services.ef.com/';
+    // For Test
+    var host = 'https://stg-efcom-lb.eflangtech.com/';  
+
+    // For Online 
+    // var host = 'https://services.ef.com/';
     var submissionURL = host + 'secureformsapi/Campaignsubmission';
     var superagent = require('superagent');
 
