4a7b73f3a0e8e4d22e1b3eb377172ebe4e403bef
liwenbin <837531387@qq.com>
2019-01-03 19:28:43 +0800

    bug fixed

--- module/wx/WXConfig.js
+++ module/wx/WXConfig.js
@@ -7,21 +7,21 @@
 // }
 
 // 英孚 订阅号
-// var WXConfig = {
-//     APPID: 'wxdf37904ad15f5f95',
-//     APPSECRET: 'a2818a5b02f7a7c93f95fc9424eeb821',
-//     token: 'icloudsoft',
-//     encodingAESKey: '0123456789ABCDEFGhijklmnopqrstuvwxyz1234567'
-// }
-
-
-// //英孚 服务号
 var WXConfig = {
-    APPID: 'wx48668a118d818bde',
-    APPSECRET: '56df55fb3dfb8de3c8776be3e621424a',
+    APPID: 'wxdf37904ad15f5f95',
+    APPSECRET: 'a2818a5b02f7a7c93f95fc9424eeb821',
     token: 'icloudsoft',
     encodingAESKey: '0123456789ABCDEFGhijklmnopqrstuvwxyz1234567'
 }
+
+
+// //英孚 服务号
+// var WXConfig = {
+//     APPID: 'wx48668a118d818bde',
+//     APPSECRET: '56df55fb3dfb8de3c8776be3e621424a',
+//     token: 'icloudsoft',
+//     encodingAESKey: '0123456789ABCDEFGhijklmnopqrstuvwxyz1234567'
+// }
 
 
 module.exports = WXConfig;
