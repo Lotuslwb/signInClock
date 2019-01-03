f28a1349d250e67c61cfa7cc343358bb6dbf0c21
liwenbin <837531387@qq.com>
2019-01-03 15:34:12 +0800

    bug fixed

--- module/wx/getSDKSign.js
+++ module/wx/getSDKSign.js
@@ -107,8 +107,8 @@ var getSDKSignFormWX = function (originalUrl, callback) {
     var getTokenUrl = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + APPID + '&secret=' + APPSECRET;
 
     load(loadWay, getTokenUrl, function (chunk) {
-        //log('获取token');
-        //log(chunk);
+        log('获取token');
+        log(chunk);
         var TOKEN = chunk.access_token;
         var expiresTime = chunk.expires_in;
 
